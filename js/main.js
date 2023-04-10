6b4316039ad4103cb09e7c9db53ede0057833bfa
Onegreat <wangreat@yandex.ru>
2023-04-10 21:58:48 +0300

    Изменение 3 и 4 блока в каталоге

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
+const btnThree = document.getElementById('btn-elem-three')
+const btnFour = document.getElementById('btn-elem-four')
 let elem3 = document.getElementById('elem-three');
 let elem4 = document.getElementById('elem-four');
 
+btnFour.onclick = function () {
+    console.log('click')
+}
+
 elem3.addEventListener('mouseover', function (event) {
     document.querySelector('.flex-container_element-three').classList.add('active');
+    btnThree.style.opacity = 100;
     elem4.style.opacity = 0;
     elem4.style.zIndex = -1;
     document.getElementById('elem-three-desc').style.visibility = 'visible';
@@ -10,6 +17,7 @@ elem3.addEventListener('mouseover', function (event) {
 
 elem3.addEventListener('mouseout', function (event) {
     document.querySelector('.flex-container_element-three').classList.remove('active');
+    btnThree.style.opacity = 0;
     elem4.style.opacity = 100;
     elem4.style.zIndex = 1;
     document.getElementById('elem-three-desc').style.visibility = 'hidden';
@@ -17,6 +25,7 @@ elem3.addEventListener('mouseout', function (event) {
 
 elem4.addEventListener('mouseover', function (event) {
     document.querySelector('.flex-container_element-four').classList.add('active');
+    btnFour.style.opacity = 100;
     elem3.style.opacity = 0;
     elem3.style.zIndex = -1;
     document.getElementById('elem-four-desc').style.visibility = 'visible';
@@ -24,6 +33,7 @@ elem4.addEventListener('mouseover', function (event) {
 
 elem4.addEventListener('mouseout', function (event) {
     document.querySelector('.flex-container_element-four').classList.remove('active');
+    btnFour.style.opacity = 0;
     elem3.style.opacity = 100;
     elem3.style.zIndex = 1;
     document.getElementById('elem-four-desc').style.visibility = 'hidden';
